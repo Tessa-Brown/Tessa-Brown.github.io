ac37d3f3c724413d7b7cb10023bfa02b8f83f6c6
Tessa-Brown <fre14004@byui.edu>
2019-03-09 12:56:10 -0800

    updated pictures

--- assignments/lesson-9/scripts/towns.js
+++ assignments/lesson-9/scripts/towns.js
@@ -23,19 +23,24 @@
             var year = document.createElement('p');
             var population = document.createElement('p');
             var rainfall = document.createElement('p');
+            var image = document.createElement('img');
     
+            var imageSrc = 'images/' + simplename + '.jpg'; 
+
             townName.textContent = towns[i].name;
             motto.textContent = towns[i].motto;
             year.textContent = 'Year Founded: ' + towns[i].yearFounded;
             population.textContent = 'Population: ' + towns[i].currentPopulation;
             rainfall.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall;
-        
+            
+            image.setAttribute('src', imageSrc);
         
             myArticle.appendChild(townName);
             myArticle.appendChild(motto);
             myArticle.appendChild(year);
             myArticle.appendChild(population);
             myArticle.appendChild(rainfall);
+            myArticle.appendChild(image);
     
             section.appendChild(myArticle);
           } 
