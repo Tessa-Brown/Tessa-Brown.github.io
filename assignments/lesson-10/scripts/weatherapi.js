fcbbe745bad7e6b2da4e1946f6d4819729f1e3c9
Tessa-Brown <fre14004@byui.edu>
2019-03-15 13:15:01 -0700

    updated api js

--- assignments/lesson-10/scripts/weatherapi.js
+++ assignments/lesson-10/scripts/weatherapi.js
@@ -1,11 +1,11 @@
 let weatherRequest = new XMLHttpRequest();
 var apiURLstring = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=68d549a21785ffa96111f8b7a8d9287f";
-request.open('GET', apiURLstring, true);
+weatherRequest.open('GET', apiURLstring, true);
 weatherRequest.send;
 
 weatherRequest.onload =  function getWeather() {
     let weatherData = JSON.parse(weatherRequest.responseText);
-    
+
     console.log(weatherData);
     document.getElementById("current-temp").innerHTML = weatherData;
 
