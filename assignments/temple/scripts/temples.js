5bd089e18bb62c6e90eabfc2049fcca492399163
Tessa-Brown <fre14004@byui.edu>
2019-04-09 15:45:35 -0700

    updated temples.json

--- assignments/temple/scripts/temples.js
+++ assignments/temple/scripts/temples.js
@@ -12,24 +12,22 @@
        
         function showTemples(jsonData) {
             var temples = jsonData['temples'];
-
-            for(var i = 0; i < temples.length; i++) {
-            
+        
 
             var closure = document.createElement('ul');
             closure.textContent = temples[i].closures;
 
-            for(var n=0; n < closures.length; n++){
-                var closure = document.createElement('li');
+            for(var n=0; n < closure.length; n++){
+                var info = document.createElement('li');
                 
-                closure.textContent = closures[n];
-                list.appendChild(closure);
+                info.textContent = closure[n];
+                list.appendChild(info);
             }
         
             section.appendChild(closure);
     
           } 
         }
-    }
+    
     
     
